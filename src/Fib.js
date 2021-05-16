178dff2499d38ce1baa97e655030bc97c2b77ef3
thanhftu <tcthanh.ftu@gmail.com>
2021-05-16 20:44:58 +0700

    styling fib to inline-block

--- src/Fib.js
+++ src/Fib.js
@@ -2,7 +2,7 @@ import Button from './components/Button'
 const Fib = ({fib, onDelete, text})=>{
     return (
         <div >
-            <h3>{fib.Value}</h3>
+            <h3 className='fib'>{fib.Value}</h3>
             <Button fib={fib} onClick={onDelete} text={text}/>
         </div>
     )
